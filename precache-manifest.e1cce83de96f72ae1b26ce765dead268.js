1ffd25ca7eb427ba7eed9c71e37bdaaec370d5d6
Miatautox <Miatautox@hotmail.com>
2020-02-22 10:50:26 -0500

    Updates

---
+++ precache-manifest.e1cce83de96f72ae1b26ce765dead268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a1f9712b15c463f8961564e23d7ba4",
+    "url": "/Contact-Manager-React-App_PRACTICE/index.html"
+  },
+  {
+    "revision": "3f0da1a3d9d3bd93039a",
+    "url": "/Contact-Manager-React-App_PRACTICE/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8550c84f80ea8559e6dd",
+    "url": "/Contact-Manager-React-App_PRACTICE/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3f0da1a3d9d3bd93039a",
+    "url": "/Contact-Manager-React-App_PRACTICE/static/js/2.85402c25.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/Contact-Manager-React-App_PRACTICE/static/js/2.85402c25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8550c84f80ea8559e6dd",
+    "url": "/Contact-Manager-React-App_PRACTICE/static/js/main.59814424.chunk.js"
+  },
+  {
+    "revision": "5c66c3178313e196018a",
+    "url": "/Contact-Manager-React-App_PRACTICE/static/js/runtime-main.24775b40.js"
+  }
+]);
